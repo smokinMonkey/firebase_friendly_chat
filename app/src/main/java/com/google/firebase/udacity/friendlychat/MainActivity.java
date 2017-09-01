4ded62223ea2a53f0bfd42b7b986859c394b3c08
Jerry Cheng <j.tc.cheng@gmail.com>
2017-09-01 15:37:33 -0700

    implemented emoji cloud function

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -16,12 +16,14 @@
 package com.google.firebase.udacity.friendlychat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,6 +36,9 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.BuildConfig;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -41,18 +46,27 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     public static final String ANONYMOUS = "anonymous";
+    public static final String FRIENDLY_MSG_LENGTH_KEY = "friendly_msg_length";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
     public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -67,6 +81,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDB;
     private DatabaseReference mMessagesDBReference;
     private ChildEventListener mChildEventListener;
+    // firebase storage
+    private FirebaseStorage mFbStorage;
+    private StorageReference mChatPhotoStorageReference;
+    // firebase remote config
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
     // firebase auth
     private FirebaseAuth mFbAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -81,10 +100,15 @@ public class MainActivity extends AppCompatActivity {
         // intialize firebase objects
         // for database
         mFirebaseDB = FirebaseDatabase.getInstance();
+        // for remote config
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         // for auth
         mFbAuth = FirebaseAuth.getInstance();
+        // for storage
+        mFbStorage = FirebaseStorage.getInstance();
 
         mMessagesDBReference = mFirebaseDB.getReference().child("messages");
+        mChatPhotoStorageReference = mFbStorage.getReference().child("chat_photos");
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -105,7 +129,11 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("image/jpeg");
+                i.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(i, "Complete action using"),
+                        RC_PHOTO_PICKER);
             }
         });
 
@@ -154,6 +182,18 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        // firebase enabling developer mode on device
+        FirebaseRemoteConfigSettings rcSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(rcSettings);
+        // create a map to map all the configuration
+        Map<String, Object> defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put(FRIENDLY_MSG_LENGTH_KEY, DEFAULT_MSG_LENGTH_LIMIT);
+        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+        // custom method to fetch remote config
+        fetchConfig();
+
         mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
         // Send button sends a message and clears the EditText
@@ -169,16 +209,64 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void fetchConfig() {
+        // set cache expiration
+        long cacheExpiration = 3600;
+        // if device is in developer mode, get the latest from firebase remote config
+        if(mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    mFirebaseRemoteConfig.activateFetched();
+                    applyRetrievedLengthLimit();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error fetching config", e);
+                    applyRetrievedLengthLimit();
+                }
+        });
+    }
+
+    private void applyRetrievedLengthLimit() {
+        Long friendly_msg_length = mFirebaseRemoteConfig.getLong(FRIENDLY_MSG_LENGTH_KEY);
+        mMessageEditText.setFilters(new InputFilter[] {
+                        new InputFilter.LengthFilter(friendly_msg_length.intValue())
+        });
+        Log.d(TAG, FRIENDLY_MSG_LENGTH_KEY + " = " + friendly_msg_length);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RC_SIGN_IN) {
-            if(resultCode == RESULT_OK) {
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
                 finish();
             }
+        }
+        else if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            // get the selected img data
+            Uri selectedImageUri = data.getData();
+            // get the filename for reference to store at chat_photos/<FILENAME>
+            StorageReference photoRef = mChatPhotoStorageReference.child(
+                    selectedImageUri.getLastPathSegment());
+            // upload file to firebaes storage
+            photoRef.putFile(selectedImageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    FriendlyMessage fm = new FriendlyMessage(null, mUsername, downloadUrl.toString());
+                    mMessagesDBReference.push().setValue(fm);
+                }
+            });
         }
     }
 
@@ -242,7 +330,10 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    FriendlyMessage fm = dataSnapshot.getValue(FriendlyMessage.class);
+                    mMessageAdapter.add(fm);
+                }
 
                 @Override
                 public void onChildRemoved(DataSnapshot dataSnapshot) { }
